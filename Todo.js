2f2c446c0ee470ab9a608ccce59e757628aee2bd
devesh kumar meena <129925418+deevsh9672@users.noreply.github.com>
2023-11-08 17:18:37 +0530

    Update Todo.js

--- Todo.js
+++ Todo.js
@@ -1,17 +1,50 @@
-let inputs = document.getElementById("inp");
-let text = document.querySelector(".text");
+// Function to add a task to the list
+function addTask() {
+  const taskInput = document.getElementById("taskInput");
+  const taskList = document.getElementById("taskList");
+  
+  const task = taskInput.value;
+  if (task === "") {
+    alert("Please enter a task!");
+    return;
+  }
 
-function label(){
-    if(inputs.value == ""){
-        alert("Please Enter Task")
-    }else{
-        let newEle = document.createElement("ul");
-        newEle.innerHTML=`${inputs.value} <i class="fa-solid fa-trash"></i>`;
-        text.appendChild(newEle);
-        inputs.value="";
-        newEle.querySelector("i").addEventListener("click" , remove);
-        function remove(){
-            newEle.remove()
-        }
-    }
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(task));
+  taskList.appendChild(li);
+
+  // Save task to local storage
+  saveTask(task);
+
+  taskInput.value = "";
+}
+
+// Function to save the task to local storage
+function saveTask(task) {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+
+  tasks.push(task);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+// Function to load tasks from local storage when the page loads
+window.onload = function () {
+  const taskList = document.getElementById("taskList");
+  let tasks;
+
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+    tasks.forEach(task => {
+      const li = document.createElement("li");
+      li.appendChild(document.createTextNode(task));
+      taskList.appendChild(li);
+    });
+  }
 }
